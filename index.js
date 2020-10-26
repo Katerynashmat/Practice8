4f5001f2bca6ca374ca5cb8f594b5ee2cd766b66
blindblackhole <niskokras2@gmail.com>
2020-10-26 20:34:04 +0200

    Get connect to database

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const port = process.env.PORT || 3000
 
 var express = require("express");
- 
 var app = express();
-
 app.use(express.static('public'));
+const db = require('./queries')
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.get('/users', db.getTable)
 
 var server = app.listen(port, function(){
     //var port = server.address().port;
---
+++ queries.js
@@ -0,0 +1,45 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+ host: 'ec2-3-95-87-221.compute-1.amazonaws.com',
+  database: 'dcsk7gi2rae78h',
+  user:'fpzbbhxabybxnt',
+  password: '0bb7a924cdfa7d3cb989f692243064ff11576caa3986fc19992ec8b8b64dcddf',
+  port: 5432, 
+  ssl: { rejectUnauthorized: false }
+}) 
+
+
+const getTable = (request, response) =>{
+    pool.query('SELECT * FROM public.student', (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+
+// const getUsers = (request, response) => {
+//   pool.query('SELECT * FROM table_sample', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+
+// const getUserById = (request, response) => {
+//   const id = parseInt(request.params.id)
+
+//   pool.query('SELECT * FROM table_sample WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+
+
+module.exports = {
+    getTable
+}
